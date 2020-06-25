313cb5d5e40a64a1de0f4af04a728a53e88a99b4
mohamed elbohy <malbohy@gmail.com>
2020-06-25 10:34:01 +0200

    stage 2

--- WebSocket/ViewController.swift
+++ WebSocket/ViewController.swift
@@ -19,8 +19,13 @@ fileprivate let VIDEO_TRACK_ID = TAG + "VIDEO"
 fileprivate let LOCAL_MEDIA_STREAM_ID = TAG + "STREAM"
 
 
+
+
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cameView: UIView!
     // MARK: - IBOutlets
     @IBOutlet weak var connectButton: UIButton!
     @IBOutlet weak var recordButton: UIButton!
@@ -28,6 +33,7 @@ class ViewController: UIViewController {
     
     var mediaStream: RTCMediaStream!
     var localAudioTrack: RTCAudioTrack!
+    
     var localVediosTrack: RTCVideoTrack!
     var dataChannel: RTCDataChannel!
     
@@ -48,6 +54,47 @@ class ViewController: UIViewController {
         
 //        webRTCClient!.setup(videoTrack: true, audioTrack: true, dataChannel: true, customFrameCapturer: false)
     }
+    
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+//        print("")
+        
+        
+        imagePickers = UIImagePickerController()
+        
+        addCameraInView()
+        
+        
+    }
+    
+    var imagePickers:UIImagePickerController?
+
+    func addCameraInView(){
+
+    imagePickers = UIImagePickerController()
+    if UIImagePickerController.isCameraDeviceAvailable( UIImagePickerController.CameraDevice.rear) {
+        imagePickers?.delegate = self
+        imagePickers?.sourceType = UIImagePickerController.SourceType.camera
+
+        //add as a childviewcontroller
+        addChild(imagePickers!)
+
+        // Add the child's View as a subview
+        self.cameView.addSubview((imagePickers?.view)!)
+        imagePickers?.view.frame = cameView.bounds
+        imagePickers?.allowsEditing = false
+        imagePickers?.showsCameraControls = false
+        imagePickers?.view.autoresizingMask = [.flexibleWidth,  .flexibleHeight]
+        }
+    }
+    
+    
+    
+    
+    
+    
     
     
     // webrtc
@@ -160,12 +207,13 @@ class ViewController: UIViewController {
         
         
         initWebRTC()
+        
         localAudioTrack = peerConnectionFactory.audioTrack(withTrackId: AUDIO_TRACK_ID)
-//        localVediosTrack = peerConnectionFactory.videoTrack(with: peerConnectionFactory.videoSource(), trackId: VIDEO_TRACK_ID)
+        localVediosTrack = peerConnectionFactory.videoTrack(with: peerConnectionFactory.videoSource(), trackId: VIDEO_TRACK_ID)
         
         mediaStream = peerConnectionFactory.mediaStream(withStreamId: LOCAL_MEDIA_STREAM_ID)
         mediaStream.addAudioTrack(localAudioTrack)
-//        mediaStream.addVideoTrack(localVediosTrack)
+        mediaStream.addVideoTrack(localVediosTrack)
 
         
         pc = prepareNewConnection()
@@ -176,16 +224,15 @@ class ViewController: UIViewController {
             pc!.add(track, streamIds: [mediaStream.streamId])
 
         }
-//        mediaStream.videoTracks.forEach { (track) in
-//            pc!.add(track, streamIds: [mediaStream.streamId])
-//        }
+        mediaStream.videoTracks.forEach { (track) in
+            pc!.add(track, streamIds: [mediaStream.streamId])
+        }
 //        let singleTrack = mediaStream.audioTracks[0] as RTCMediaStreamTrack
         
         
         
         let mandatoryConstraints = ["OfferToReceiveAudio": "true", "OfferToReceiveVideo": "false"]
         let optionalConstraints = [ "DtlsSrtpKeyAgreement": "true", "RtpDataChannels" : "true", "internalSctpDataChannels" : "true"]
-        
         
         
         pc!.offer(for: RTCMediaConstraints(mandatoryConstraints: mandatoryConstraints, optionalConstraints: optionalConstraints)) { (description, error) in
@@ -204,6 +251,7 @@ class ViewController: UIViewController {
             print("offer has error : \(error)")
             
             
+            // try again with codec : h264
             let data = ["name":"kbqdm648", "sdp":description!.sdp, "codec":"vp8"]
             
             let transID = Double.random(in: 0...1) * 10000
@@ -524,3 +572,9 @@ extension ViewController: RTCPeerConnectionDelegate{
 
 
 //{"type":"cmd","transId":4067.4135088288567,"name":"publish","data":{"name":"kbqdm648","sdp":"v=0\r\no=- 292404632969147590 2 IN IP4 127.0.0.1\r\ns=-\r\nt=0 0\r\na=group:BUNDLE 0 1\r\na=msid-semantic: WMS 1AXF8Ht1VPB6S5SorvDtsnRzqiDgJDCzVWcE\r\nm=audio 9 UDP/TLS/RTP/SAVPF 111 103 104 9 0 8 106 105 13 110 112 113 126\r\nc=IN IP4 0.0.0.0\r\na=rtcp:9 IN IP4 0.0.0.0\r\na=ice-ufrag:Wpl4\r\na=ice-pwd:abE1zfuYgO8/AaJH3EN4jNLR\r\na=ice-options:trickle\r\na=fingerprint:sha-256 42:7F:88:89:30:54:06:66:E7:6B:38:58:AD:34:44:DF:91:03:42:F5:9B:04:CC:22:01:66:F6:DA:16:99:BC:17\r\na=setup:actpass\r\na=mid:0\r\na=extmap:1 urn:ietf:params:rtp-hdrext:ssrc-audio-level\r\na=extmap:2 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time\r\na=extmap:3 http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01\r\na=extmap:4 urn:ietf:params:rtp-hdrext:sdes:mid\r\na=extmap:5 urn:ietf:params:rtp-hdrext:sdes:rtp-stream-id\r\na=extmap:6 urn:ietf:params:rtp-hdrext:sdes:repaired-rtp-stream-id\r\na=sendrecv\r\na=msid:1AXF8Ht1VPB6S5SorvDtsnRzqiDgJDCzVWcE d62ab2ff-8fc9-42cd-9f4c-4fcccdd1a676\r\na=rtcp-mux\r\na=rtpmap:111 opus/48000/2\r\na=rtcp-fb:111 transport-cc\r\na=fmtp:111 minptime=10;useinbandfec=1\r\na=rtpmap:103 ISAC/16000\r\na=rtpmap:104 ISAC/32000\r\na=rtpmap:9 G722/8000\r\na=rtpmap:0 PCMU/8000\r\na=rtpmap:8 PCMA/8000\r\na=rtpmap:106 CN/32000\r\na=rtpmap:105 CN/16000\r\na=rtpmap:13 CN/8000\r\na=rtpmap:110 telephone-event/48000\r\na=rtpmap:112 telephone-event/32000\r\na=rtpmap:113 telephone-event/16000\r\na=rtpmap:126 telephone-event/8000\r\na=ssrc:82668464 cname:o8Ul5+BnprjMXlym\r\na=ssrc:82668464 msid:1AXF8Ht1VPB6S5SorvDtsnRzqiDgJDCzVWcE d62ab2ff-8fc9-42cd-9f4c-4fcccdd1a676\r\na=ssrc:82668464 mslabel:1AXF8Ht1VPB6S5SorvDtsnRzqiDgJDCzVWcE\r\na=ssrc:82668464 label:d62ab2ff-8fc9-42cd-9f4c-4fcccdd1a676\r\nm=video 9 UDP/TLS/RTP/SAVPF 96 97 98 99 100 101 102 122 127 121 125 107 108 109 124 120 123 119 114 115 116\r\nc=IN IP4 0.0.0.0\r\na=rtcp:9 IN IP4 0.0.0.0\r\na=ice-ufrag:Wpl4\r\na=ice-pwd:abE1zfuYgO8/AaJH3EN4jNLR\r\na=ice-options:trickle\r\na=fingerprint:sha-256 42:7F:88:89:30:54:06:66:E7:6B:38:58:AD:34:44:DF:91:03:42:F5:9B:04:CC:22:01:66:F6:DA:16:99:BC:17\r\na=setup:actpass\r\na=mid:1\r\na=extmap:14 urn:ietf:params:rtp-hdrext:toffset\r\na=extmap:2 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time\r\na=extmap:13 urn:3gpp:video-orientation\r\na=extmap:3 http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01\r\na=extmap:12 http://www.webrtc.org/experiments/rtp-hdrext/playout-delay\r\na=extmap:11 http://www.webrtc.org/experiments/rtp-hdrext/video-content-type\r\na=extmap:7 http://www.webrtc.org/experiments/rtp-hdrext/video-timing\r\na=extmap:8 http://tools.ietf.org/html/draft-ietf-avtext-framemarking-07\r\na=extmap:9 http://www.webrtc.org/experiments/rtp-hdrext/color-space\r\na=extmap:4 urn:ietf:params:rtp-hdrext:sdes:mid\r\na=extmap:5 urn:ietf:params:rtp-hdrext:sdes:rtp-stream-id\r\na=extmap:6 urn:ietf:params:rtp-hdrext:sdes:repaired-rtp-stream-id\r\na=sendrecv\r\na=msid:1AXF8Ht1VPB6S5SorvDtsnRzqiDgJDCzVWcE a1974d14-387b-4d26-b1a8-7b4dc9c45140\r\na=rtcp-mux\r\na=rtcp-rsize\r\na=rtpmap:96 VP8/90000\r\na=rtcp-fb:96 goog-remb\r\na=rtcp-fb:96 transport-cc\r\na=rtcp-fb:96 ccm fir\r\na=rtcp-fb:96 nack\r\na=rtcp-fb:96 nack pli\r\na=rtpmap:97 rtx/90000\r\na=fmtp:97 apt=96\r\na=rtpmap:98 VP9/90000\r\na=rtcp-fb:98 goog-remb\r\na=rtcp-fb:98 transport-cc\r\na=rtcp-fb:98 ccm fir\r\na=rtcp-fb:98 nack\r\na=rtcp-fb:98 nack pli\r\na=fmtp:98 profile-id=0\r\na=rtpmap:99 rtx/90000\r\na=fmtp:99 apt=98\r\na=rtpmap:100 VP9/90000\r\na=rtcp-fb:100 goog-remb\r\na=rtcp-fb:100 transport-cc\r\na=rtcp-fb:100 ccm fir\r\na=rtcp-fb:100 nack\r\na=rtcp-fb:100 nack pli\r\na=fmtp:100 profile-id=2\r\na=rtpmap:101 rtx/90000\r\na=fmtp:101 apt=100\r\na=rtpmap:102 H264/90000\r\na=rtcp-fb:102 goog-remb\r\na=rtcp-fb:102 transport-cc\r\na=rtcp-fb:102 ccm fir\r\na=rtcp-fb:102 nack\r\na=rtcp-fb:102 nack pli\r\na=fmtp:102 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42001f\r\na=rtpmap:122 rtx/90000\r\na=fmtp:122 apt=102\r\na=rtpmap:127 H264/90000\r\na=rtcp-fb:127 goog-remb\r\na=rtcp-fb:127 transport-cc\r\na=rtcp-fb:127 ccm fir\r\na=rtcp-fb:127 nack\r\na=rtcp-fb:127 nack pli\r\na=fmtp:127 level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=42001f\r\na=rtpmap:121 rtx/90000\r\na=fmtp:121 apt=127\r\na=rtpmap:125 H264/90000\r\na=rtcp-fb:125 goog-remb\r\na=rtcp-fb:125 transport-cc\r\na=rtcp-fb:125 ccm fir\r\na=rtcp-fb:125 nack\r\na=rtcp-fb:125 nack pli\r\na=fmtp:125 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f\r\na=rtpmap:107 rtx/90000\r\na=fmtp:107 apt=125\r\na=rtpmap:108 H264/90000\r\na=rtcp-fb:108 goog-remb\r\na=rtcp-fb:108 transport-cc\r\na=rtcp-fb:108 ccm fir\r\na=rtcp-fb:108 nack\r\na=rtcp-fb:108 nack pli\r\na=fmtp:108 level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=42e01f\r\na=rtpmap:109 rtx/90000\r\na=fmtp:109 apt=108\r\na=rtpmap:124 H264/90000\r\na=rtcp-fb:124 goog-remb\r\na=rtcp-fb:124 transport-cc\r\na=rtcp-fb:124 ccm fir\r\na=rtcp-fb:124 nack\r\na=rtcp-fb:124 nack pli\r\na=fmtp:124 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=4d0032\r\na=rtpmap:120 rtx/90000\r\na=fmtp:120 apt=124\r\na=rtpmap:123 H264/90000\r\na=rtcp-fb:123 goog-remb\r\na=rtcp-fb:123 transport-cc\r\na=rtcp-fb:123 ccm fir\r\na=rtcp-fb:123 nack\r\na=rtcp-fb:123 nack pli\r\na=fmtp:123 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=640032\r\na=rtpmap:119 rtx/90000\r\na=fmtp:119 apt=123\r\na=rtpmap:114 red/90000\r\na=rtpmap:115 rtx/90000\r\na=fmtp:115 apt=114\r\na=rtpmap:116 ulpfec/90000\r\na=ssrc-group:FID 4138666451 3080902777\r\na=ssrc:4138666451 cname:o8Ul5+BnprjMXlym\r\na=ssrc:4138666451 msid:1AXF8Ht1VPB6S5SorvDtsnRzqiDgJDCzVWcE a1974d14-387b-4d26-b1a8-7b4dc9c45140\r\na=ssrc:4138666451 mslabel:1AXF8Ht1VPB6S5SorvDtsnRzqiDgJDCzVWcE\r\na=ssrc:4138666451 label:a1974d14-387b-4d26-b1a8-7b4dc9c45140\r\na=ssrc:3080902777 cname:o8Ul5+BnprjMXlym\r\na=ssrc:3080902777 msid:1AXF8Ht1VPB6S5SorvDtsnRzqiDgJDCzVWcE a1974d14-387b-4d26-b1a8-7b4dc9c45140\r\na=ssrc:3080902777 mslabel:1AXF8Ht1VPB6S5SorvDtsnRzqiDgJDCzVWcE\r\na=ssrc:3080902777 label:a1974d14-387b-4d26-b1a8-7b4dc9c45140\r\n","codec":"h264"}}
+
+
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+}
